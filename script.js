eb550a120c1c9678f0ab86d61b2099541f903257
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-04-10 22:13:28 +0530

    commit on 'master'

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const addForm=document.getElementById('form');
+console.log(addForm);
+addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    var fill=addForm.querySelector('input[type="text"]').value;
+    var hill=addForm.querySelector('input[type="url"]').value;
+    console.log(fill);
+    console.log(hill);
+})
+
 
 
 
