484a2fa6ea6e48834dcd8cd9a232b1d050ec2aa8
Shobhit Singh <shobhitsingh25032003@gmail.com>
2022-04-18 09:38:34 +0530

    commit on 'master'

--- script.js
+++ script.js
@@ -17,32 +17,22 @@ console.log(Array.isArray(neon));
 //     neon[i].appendChild(contentDiv);
 // }
 
-// for(i = 0; i < 7){
-//     for(j = 0; j < 8){
-//         document.getElementById(`${i}${j}`)
-//         localStorage.getItem(`${i}${j}`)
+for(i = 0; i < 7){
+    for(j = 0; j < 8){
+        document.getElementById(`${i}${j}`)
+        localStorage.getItem(`${i}${j}`)
 
-//     }
-// }
-                        /*
-                        1) how will we add specific data storage indexes(by key and value of entered parameters)
-                        2) also I have to do this specifically on that submit button which i have preseed so why do i need getElementById('$(i)$(j)')
-                        3)** what if I add individual forms to open when a button is pressed and then take data from that and save that particular data in local Storage so that it doesnt changes when reloaded. and add a reset button with function to reset it or edit it?**
-                        5) woiuld that be feasible?
-                        6) explain the second method again in brief.
-                        */
-
-
-//
-const arr = [];
-
-for(let i = 0; i < 2; i++){
-    const arr2 = [];
-    for(let j = 0; j < 3; j++){
-        arr2.push({name: 'shobhit', link: 'https://www.google.com'});
     }
-    arr.push(arr2);
-}
+// }
+// const arr = [];
+
+// for(let i = 0; i < 2; i++){
+//     const arr2 = [];
+//     for(let j = 0; j < 3; j++){
+//         arr2.push({name: 'shobhit', link: 'https://www.google.com'});
+//     }
+//     arr.push(arr2);
+// }
 
 // localStorage.setItem('timetable', JSON.stringify(arr));
 
